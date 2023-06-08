4da1312218eb5ce5703415ed5a9c125cf03523b1
wendellast <wendelalves02a@hotmail.com>
2023-06-08 17:52:39 -0300

    Ajustes e pequenas melhorias

--- js/clr.js
+++ js/clr.js
@@ -1,13 +1,13 @@
-function adjustButtonTextOnScreenSize() {
-    var screenWidth = window.innerWidth;
-    var Button = document.getElementById('opco');
-  
-    if (screenWidth < 767) {
-      Button.textContent = '☰';
-    } else {
-      Button.textContent = 'Menu';
-    }
+function adjustButtonTextOnScreenSize () {
+  var screenWidth = window.innerWidth;
+  var Button = document.getElementById('opco');
+
+  if (screenWidth < 767) {
+    Button.textContent = '☰';
+  } else {
+    Button.textContent = 'Menu';
+  }
 }
-  
+
 window.addEventListener('resize', adjustButtonTextOnScreenSize);
-  
+
